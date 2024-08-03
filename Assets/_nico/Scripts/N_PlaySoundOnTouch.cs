da14140f2af57fe5b8c78ba00780944d09b757ff
Nico <syntonikka@gmail.com>
2024-08-02 18:49:10 -0700

    added instruments with sounds, lotus sets, rocks

--- Assets/_nico/Scripts/N_PlaySoundOnTouch.cs
+++ Assets/_nico/Scripts/N_PlaySoundOnTouch.cs
@@ -7,10 +7,22 @@ using UnityEngine;
 public class N_PlaySoundOnTouch : MonoBehaviour
 {
     public AudioSource audio;
+    public string targetStringContains = "Hand";
+    public GameObject collidingObject;
+
+    private bool isCooldown = false;
+    private float cooldownTime = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        
+    }
+
+    void Awake(){
         audio = GetComponent<AudioSource>();
+        audio.playOnAwake = false;
     }
 
     // Update is called once per frame
@@ -21,6 +33,23 @@ public class N_PlaySoundOnTouch : MonoBehaviour
 
     public void PlayAudio()
     {
+        //check if in cooldown period, if so, do nothing
+        if (isCooldown) return;
         audio.Play();
+        StartCoroutine(Cooldown());
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name.Contains(targetStringContains)){
+            collidingObject = collision.gameObject;
+            PlayAudio();
+        }
+    }
+
+    private IEnumerator Cooldown()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        isCooldown = false;
     }
 }
